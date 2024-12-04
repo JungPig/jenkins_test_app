fe4856c1acc0f78ab2018e6fec75c9307d692c7d
jungpig <pig.ohgiraffers@gmail.com>
2024-12-04 13:06:01 +0900

    test2

--- src/main/java/com/ohgiraffers/jenkins_test_app/JenkinsTestAppApplication.java
+++ src/main/java/com/ohgiraffers/jenkins_test_app/JenkinsTestAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JenkinsTestAppApplication {
-
+    // test
     public static void main(String[] args) {
         SpringApplication.run(JenkinsTestAppApplication.class, args);
     }
