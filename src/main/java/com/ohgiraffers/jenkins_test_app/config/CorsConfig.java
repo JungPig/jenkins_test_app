e6478b85ad970ecd7f780768e79bade33175e062
jungpig <pig.ohgiraffers@gmail.com>
2024-12-05 12:39:28 +0900

    test

--- src/main/java/com/ohgiraffers/jenkins_test_app/config/CorsConfig.java
+++ src/main/java/com/ohgiraffers/jenkins_test_app/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 설정
-                .allowedOrigins("*") // 허용할 도메인
+                .allowedOrigins("https://cloud-test-app.vercel.app") // 허용할 도메인
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 모든 헤더 허용
                 .allowCredentials(true); // 쿠키 허용
